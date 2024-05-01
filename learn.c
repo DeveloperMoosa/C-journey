6e79d1425d6e700ffc48fec5ca422379d89bab87
DeveloperMoosa <161608892+DeveloperMoosa@users.noreply.github.com>
2024-05-01 22:33:45 +0500

    Add files via upload

--- learn.c
+++ learn.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main(){
-// The different ways to code multiple   data types 
-	int age = 20;
-	float pi = 3.14;          //the value of pi
-	char percentage = '%'; // the character will also want to be coded in single quotes
+ 
+ // variables
+  int number;
+  int name;
+  int prices;
 
 return 0;
 }
