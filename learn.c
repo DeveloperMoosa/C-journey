6608b55cad257fcd31edcc6bd5dae76062e1c03c
DeveloperMoosa <161608892+DeveloperMoosa@users.noreply.github.com>
2024-05-01 22:29:44 +0500

    Add files via upload
    Data types

--- learn.c
+++ learn.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-// these are the types of variables that can be coded for different variables
-	int  number;
-	int name;
-	int ages;
+// The different ways to code multiple   data types 
+	int age = 20;
+	float pi = 3.14;          //the value of pi
+	char percentage = '%'; // the character will also want to be coded in single quotes
 
 return 0;
 }
