c4cbf12f40ef0352c6b0cf3f7a50fc861277aca9
DeveloperMoosa <161608892+DeveloperMoosa@users.noreply.github.com>
2024-05-02 12:38:19 +0500

    Add files via upload
    adding to numbers

---
+++ summing.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the first number : ");
+    scanf("%d", &a);
+    printf("enter th second number :");
+    scanf("%d", &b);
+
+    printf("the sum of both numbers %d", a+b);
+
+}
