2ea637fe16635ecf4a4ba9b23108e35cd22c3bf7
DeveloperMoosa <161608892+DeveloperMoosa@users.noreply.github.com>
2024-05-06 22:57:32 +0500

    Add files via upload
    declaring the functions

---
+++ function.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+    //Decleration of function
+void printhello();
+    // calling the declared function
+int main(){
+    printhello();
+    return 0;
+}
+    //Defining the function
+void printhello(){
+    printf("Hello world\n");
+}
